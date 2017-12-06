aad1b56a2aea70a4888917804fae4969b41f90e4
jiayu2B <jiayukmifeng@qq.com>
2017-12-06 10:54:12 -0800

    Create Next Permutation.py

---
+++ Next Permutation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <=1:
+            return
+        for i in range (len(nums)-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                
+                for k in range(len(nums)-1,i,-1):
+                    if nums[k] > nums[i]: 
+                        nums[i],nums[k]=nums[k],nums[i]
+                        nums[i+1:]=sorted(nums[i+1:])
+                        break
+                break
+            else:
+                if i == 0:
+                    nums.sort()
+
