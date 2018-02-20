7114c97cc23f1051ddf64e348a2edce850696561
jiayu2B <jiayukmifeng@qq.com>
2018-02-19 16:38:44 -0800

    Update 64_ Minimum Path Sum.py

--- 64_ Minimum Path Sum.py
+++ 64_ Minimum Path Sum.py
@@ -12,8 +12,10 @@ class Solution(object):
             arr[i][0] = arr[i-1][0] + grid[i][0]
         for j in range(1,n):
             arr[0][j] = arr[0][j-1] +grid[0][j]
+        #初始化第一列第一行。
+            
         for i in range(1,m):
             for j in range(1,n):
                 arr[i][j] = min( arr[i-1][j] + grid[i][j], arr[i][j-1] + grid[i][j])
-                
+                #动态规划，每一个格子最小的值为上左各自最小值加本值
         return arr[-1][-1]
